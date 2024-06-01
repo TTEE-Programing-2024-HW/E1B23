d8fa605c54458e00c9fe5a6dd8a41fb9e2c17c48
E1B23 <u11202223@o365.ttu.edu.tw>
2024-06-01 13:21:32 +0800

    將第一步印出個人風格畫面設計完畢

---
+++ HW4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//主程式 
+int main() {
+	//個人風格畫面 
+	printf("*                  \n");
+    printf("**                 \n");
+    printf("***                \n");
+    printf("****               \n");
+    printf("*****              \n");
+    printf(" *****             \n");
+    printf("  *****            \n");
+    printf("   *****           \n");
+    printf("    *****          \n");
+    printf("     *****         \n");
+    printf("      *****        \n");
+    printf("       *****       \n");
+    printf("        *****      \n");
+    printf("         *****     \n");
+    printf("          *****    \n");
+    printf("           *****   \n");
+    printf("            *****  \n");
+    printf("             ***** \n");
+    printf("              *****\n");
+    printf("               ****\n");
+    printf("                ***\n");
+    printf("                 **\n");
+    printf("                  *\n");
+    
+    return 0;
+}
